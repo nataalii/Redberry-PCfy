bc0ee594e7fbb13b1d779919d4a2d3793b766fd6
nnataali <99037577+nnataali@users.noreply.github.com>
2022-08-28 20:42:58 +0400

    add staff info validation

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-//this button returns staff info page to the home page
-const backBtn = document.getElementById('back-btn')
-const addItemBtn = document.querySelector('#add-item-btn')
-backBtn.onclick = function() {
-    location.href = '../index.html'
-}
-
-
 //teams api
 const selectDrop = document.querySelector('.option');
 const selectPos = document.querySelector('.positions-option')
@@ -31,7 +23,7 @@ fetch('https://pcfy.redberryinternship.ge/api/teams').then(res => {
 // teams and positions dropdown list
 
 function show(value) {
-    document.querySelector('.positionTextBox').value = 'პოზიცია';
+    document.querySelector('.positionTextBox').value = '';
     document.querySelector('.textBox').value = value
 
     arr.forEach(item => {
@@ -73,3 +65,103 @@ teamsDropdown.onclick = function() {
 positionsDropdown.onclick = function() {
     positionsDropdown.classList.toggle('active')
 }
+
+
+// staff info form validation
+const form = document.querySelector('#form')
+const firstName = document.getElementById('name')
+const lastName = document.getElementById('lastname')
+const email = document.getElementById('email')
+const phone = document.getElementById('telephone')
+const btn = document.getElementById('next-btn')
+const team = document.getElementById('team')
+const position = document.getElementById('position')
+const geoAlph = /^[ა-ჰ]+$/;
+const engAlph = /^[a-zA-Z0-9@.]+$/;
+// const phoneNum = /^[\+]?[995]?[5]?[0-9]{2}[-\s\.]?[0-9]{2}[-\s\.]?[0-9]{2}[-\s\.]?[0-9]{2}$/
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    validateInputs();
+    
+})
+
+const validateInputs = () => {
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const teamValue = team.value
+    const positionValue = position.value
+    const emailValue = email.value.trim()
+    const phoneValue = phone.value.trim()
+
+    // firstname validation
+    if(firstNameValue.length < 2){
+        setError(firstName, 'სახელი უნდა შეიცავდეს მინიმუმ 2 სიმბოლოს')
+    } else if(!geoAlph.test(firstNameValue)){
+        setError(firstName, 'სახელი უნდა შეიცავდეს ქართულ ასოებს')
+    } else {
+        setSuccess(firstName)
+    }
+
+    // lastname validation
+    if(lastNameValue.length < 2){
+        setError(lastName, 'გვარი უნდა შეიცავდეს მინიმუმ 2 სიმბოლოს')
+    } else if(!geoAlph.test(lastNameValue)){
+        setError(lastName, 'სახელი უნდა შეიცავდეს ქართულ ასოებს')
+    } else {
+        setSuccess(lastName)
+    }
+
+    // team validation
+    if(teamValue === ''){
+        setError(team, '')
+    } else {
+        setSuccess(team)
+    }
+
+    // position validation
+    if(positionValue === ''){
+        setError(position, '')
+    } else {
+        setSuccess(position)
+    }
+
+    // email validation
+    if(emailValue === ''){
+        setError(email, 'მეილის შეყვანა სავალდებულოა');
+    } else if((emailValue.endsWith("@redberry.ge") && emailValue.length === 12) ||
+       !engAlph.test(emailValue) ){
+        setError(email, 'შეიყვანეთ მეილი სწორი ფორმატით')
+    } else if(!emailValue.endsWith("@redberry.ge")) {
+        setError(email, 'მეილი უნდა მთავრდებოდეს @redberry.ge-ით');
+    } else {
+        setSuccess(email);
+    }
+
+    // telephone number validation
+    if(phoneValue.length != 9){
+        setError(phone, 'უნდა აკმაყოფილებდეს ქართული მობ-ნომრის ფორმატს');
+    } else {
+        setSuccess(phone);
+    }
+
+    
+
+}
+const setError = (element, message) => {
+     const inputControl = element.parentElement;
+     const errorDisplay = inputControl.querySelector('.error-message')
+
+     errorDisplay.innerText = message
+     inputControl.classList.add('error');
+    //  inputControl.classList.remove('success')
+}
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error-message')
+    errorDisplay.innerText = ''
+    // inputControl.classList.add('success');
+    inputControl.classList.remove('error')
+}
+
