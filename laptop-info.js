6faeb1799130f6462978d8da977f3ece24aac837
nnataali <99037577+nnataali@users.noreply.github.com>
2022-08-29 16:45:39 +0400

    laptop info page design

--- laptop-info.js
+++ laptop-info.js
@@ -52,3 +52,72 @@ function showFile(){
         dropArea.classList.remove('active')
     }
 }
+
+
+// //teams api
+// const selectDrop = document.querySelector('.option');
+// const selectPos = document.querySelector('.positions-option')
+// let arr = []
+// fetch('https://pcfy.redberryinternship.ge/api/teams').then(res => {
+//     return res.json();
+// }).then(finalData => {
+//     let output = '';
+//     finalData.data.forEach(team => {
+//         output += `<div onclick="show('${team.name}')">${team.name}</div>`
+//         const obj = {
+//             id: team.id,
+//             name: team.name
+//         }
+//         arr.push(obj)
+//     })
+//     selectDrop.innerHTML = output
+// }).catch(err => {
+//     console.log(err)
+// })
+
+
+// // teams and positions dropdown list
+
+// function show(value) {
+//     document.querySelector('.positionTextBox').value = '';
+//     document.querySelector('.textBox').value = value
+
+//     arr.forEach(item => {
+//         if(item.name === value){
+//             fetch('https://pcfy.redberryinternship.ge/api/positions').then(res => {
+//                 return res.json()
+//             }).then(finalData => {
+//                 let output = ''
+//                 const filterdArray = finalData.data.filter(position => position.team_id === item.id)
+//                 filterdArray.forEach(position => {
+//                     output += `<div onclick="showPositions('${position.name}')">${position.name}</div>`
+//                 })
+//                 selectPos.innerHTML = output
+                
+//             }).catch(err => {
+//                 console.log(err)
+//             })
+//         } 
+
+//     })
+
+
+// }
+
+// function showPositions(value) {
+//     document.querySelector('.positionTextBox').value = value;
+// }
+
+
+
+
+// let teamsDropdown = document.querySelector('.teams-dropdown')
+// let positionsDropdown = document.querySelector('.positions-dropdown')
+
+// teamsDropdown.onclick = function() {
+//     teamsDropdown.classList.toggle('active')
+// }
+
+// positionsDropdown.onclick = function() {
+//     positionsDropdown.classList.toggle('active')
+// }
